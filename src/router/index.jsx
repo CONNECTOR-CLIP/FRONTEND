b1bfc458884078da0a9ddb19890ec5eac41ba8ec
이원재 <lwoj3019515@gmail.com>
2026-04-03 22:35:07 +0900

    Feat: 로그인페이지 기초세팅
    로그인 페이지 진입을 우선 쉽게 하기 위해 세팅 및 기초 작업

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "@/App";
+import Home from "@/pages/Home";
+import Login from "@/pages/login";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [],
+    children: [
+      { index: true, element: <Home /> },
+      { path: "login", element: <Login /> },
+    ],
   },
 ]);
