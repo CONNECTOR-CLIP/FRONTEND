d5d8a408822198e70d97b55e55c813ee11068645
이원재 <lwoj3019515@gmail.com>
2026-04-04 18:01:40 +0900

    fix:랜딩페이지 투명도 수정

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -6,8 +6,8 @@ function MainLayout() {
     <div className="relative min-h-screen overflow-hidden">
       {/* bg blur orbs */}
       <div className="pointer-events-none absolute inset-0 -z-10">
-        <div className="absolute -top-10 -left-20 w-[500px] h-[500px] rounded-full bg-[#bfdbfe] opacity-50 blur-[120px]" />
-        <div className="absolute top-80 right-0 w-[450px] h-[450px] rounded-full bg-[#ddd6fe] opacity-40 blur-[120px]" />
+        <div className="absolute -top-10 -left-20 w-[500px] h-[500px] rounded-full bg-[#bfdbfe] opacity-50 blur-[30px]" />
+        <div className="absolute top-80 right-0 w-[450px] h-[450px] rounded-full bg-[#ddd6fe] opacity-40 blur-[30px]" />
       </div>
 
       <Navbar />
